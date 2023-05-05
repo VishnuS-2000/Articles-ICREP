ca6d6d63ce8291215c316f9c281de23ee1701141
VishnuS-2000 <vishnusatish2000@gmail.com>
2023-05-05 12:14:03 +0530

    update publication info

--- client/components/NewsNotifications.jsx
+++ client/components/NewsNotifications.jsx
@@ -14,16 +14,14 @@ export const NewsContainer=()=>{
 
 
 
-    return <div className="py-5  flex flex-col rounded-md bg-slate-50 w-full  w-full desktop:max-w-[20%] relative">
+    return <div className="py-5 desktop:py-20  flex flex-col rounded-md bg-slate-50 w-full  w-full desktop:max-w-[20%] relative">
                 <h1 className="flex px-5 items-center space-x-3  text-base desktop:text-base desktop:text-base font-[500] ">
                 
 
                     <span>
                     News & Notifications
                         </span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 desktop:w-6 desktop:h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
-</svg>
+                        
                         </h1>
 
 
--- client/components/PublicationInfo.jsx
+++ client/components/PublicationInfo.jsx
@@ -35,7 +35,7 @@ export const PublicationsInfoContainer=()=>{
 
 
 
-    return <div className="py-5  flex flex-col rounded-md bg-slate-50 w-full  w-full desktop:max-w-[20%] relative">
+    return <div className="py-5 desktop:py-20  flex flex-col rounded-md bg-slate-50 w-full  w-full desktop:max-w-[20%] relative">
                 <h1 className="flex px-6 items-center space-x-3  text-base  desktop:text-sm font-[500] ">
                 
 
@@ -47,7 +47,7 @@ export const PublicationsInfoContainer=()=>{
         <div className='px-5 my-2'>
                 {issues?.map((element,index)=>{
 
-                 return <Link key={index} href={`/search?issue=${element?.issue}&#results`} className='flex items-center p-2 rounded-md hover:bg-slate-200  justify-between duration-500 text-sm desktop:text-base '>
+                 return <Link key={index} href={`/publications?issue=${element?.issue}`} className='flex items-center p-2 rounded-md hover:bg-slate-200  justify-between duration-500 text-sm desktop:text-base '>
                        {`Issue ${element?.issue}`} 
                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
